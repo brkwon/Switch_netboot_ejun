70b98dc7f19eafcf5fbbafb60b382d8db2e15925
root <root@tftpserver.(none)>
2014-02-07 12:59:49 +0900

    update

---
+++ update_conf/webUI/build_tor_config.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import cgi
+import cgitb
+import os
+import manage
+
+class Register_zone_aggrs:
+
+ def __init__(self):
+
+  cgitb.enable()
+  self.form = cgi.FieldStorage()
+  self.base_html = open("./base.html").read()
+  self.insert_contents={}
+  self.insert_contents['page_name']=" Create the Switch Configuration "
+  form_content =  open("./build_tor_config.form").read()
+  self.insert_contents['form_name']=form_content
+
+
+ def variables_empty_status(self, *args):
+  status=True
+  for arg in args:
+   if not arg:
+    status=False
+    break
+  return status
+ 
+ def display_html(self):
+  print "Content-Type: text/html\n\n"
+  print self.base_html % self.insert_contents
+
+
+if __name__ == "__main__":
+ obj = Register_zone_aggrs()
+ obj.display_html()
+ 
+  
